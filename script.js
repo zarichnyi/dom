6f0b4098627ca5e592a30ca492073fb6da5f79f1
Vasyl <vasylzarichnyi@gmail.com>
2016-08-31 00:15:29 +0300

    upd

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
- 
-            
-            document.body.style.background= background;
-            document.body.style.fontFamily= font;
-            document.body.firstElementChild.style.textAlign= align;
-            document.body.children[1].style.background= fon;
+window.onclick = function (){
+var background = prompt('фон');
+        var font = prompt('шрифт');
+var align = prompt('вирівнювання h1');
+var fon = prompt('фон параграфа');
+var color = prompt('link color');
+var div = prompt('color div');
+var div1 = prompt('size div');
+var div2 = prompt('font-weight div');
+var liststyle = prompt('list');
+var setlink1 = prompt('setlink1');
+var setlink2 = prompt('setlink2');
+var setlink3 = prompt('setlink3');
+}
+            document.body.style.background = background;
+            document.body.style.fontFamily = font;
+            document.body.firstElementChild.style.textAlign = align;
+            document.body.children[1].style.background = fon;
 
             var child = document.getElementsByTagName("a");
 
@@ -23,16 +35,3 @@
         document.body.children[1].children[2].innerHTML = setlink3;
         document.body.children[1].children[2].href ='http://' + setlink3;
         document.body.children[1].children[2].target = '_blank';
-
-var background = prompt('фон');
-        var font = prompt('шрифт');
-var align = prompt('вирівнювання h1');
-var fon = prompt('фон параграфа');
-var color = prompt('link color');
-var div = prompt('color div');
-var div1 = prompt('size div');
-var div2 = prompt('font-weight div');
-var liststyle = prompt('list');
-var setlink1 = prompt('setlink1');
-var setlink2 = prompt('setlink2');
-var setlink3 = prompt('setlink3');
